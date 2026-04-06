9b7643b2b01359ef0c211bb5088906c4be56bb91
aleegator <aleninja774@gmail.com>
2026-04-05 23:37:08 -0400

    upload workspace for firebase

---
+++ public/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOPLJMhpsCFlrF5V1VOCohu8Mlthapo9U",
+  authDomain: "dfarm1.firebaseapp.com",
+  projectId: "dfarm1",
+  storageBucket: "dfarm1.firebasestorage.app",
+  messagingSenderId: "585544527682",
+  appId: "1:585544527682:web:ace16b2c979036019f22db"
+};
+
+export default firebaseConfig
